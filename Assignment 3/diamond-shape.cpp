76b96a3a287d1d2f8337f11204f96a841c55b185
Sabih Haider <121865932+sabih-haider1@users.noreply.github.com>
2023-01-27 13:09:02 +0500

    Update diamond-shape.cpp

--- Assignment 3/diamond-shape.cpp
+++ Assignment 3/diamond-shape.cpp
@@ -1,39 +1,39 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//    int n,i,j,k,x,dec,a,b,c;
-//    cout << "Enter number of rows (rows < 10) = ";
-//    cin >> n;
-//    x=n/2;
-//    dec=n;
-//    cout << endl;
-//    for (i = 0; i < x; i++)
-//    {
-//            for (j = 0; j < x - i; j++)
-//            {
-//                cout << " ";
-//            }
-//            for (k = 1; k < i*2; k++)
-//            {
-//                cout << k;
-//            }
-//            cout << endl;
-//    }
-//    for(a=0;a<x;a++)
-//    {
-//        for(b=1;b<dec;b++)
-//        {
-//            cout<<b;
-//        }
-//        cout << endl;
-//        dec=dec-2;
-//        for(c=0;c<=a;c++)
-//        {
-//            cout<<" ";
-//        }
-//    }
-//    system("pause");
-//    return 0;
-//}
-//
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n,i,j,k,x,dec,a,b,c;
+   cout << "Enter number of rows (rows < 10) = ";
+   cin >> n;
+   x=n/2;
+   dec=n;
+   cout << endl;
+   for (i = 0; i < x; i++)
+   {
+           for (j = 0; j < x - i; j++)
+           {
+               cout << " ";
+           }
+           for (k = 1; k < i*2; k++)
+           {
+               cout << k;
+           }
+           cout << endl;
+   }
+   for(a=0;a<x;a++)
+   {
+       for(b=1;b<dec;b++)
+       {
+           cout<<b;
+       }
+       cout << endl;
+       dec=dec-2;
+       for(c=0;c<=a;c++)
+       {
+           cout<<" ";
+       }
+   }
+   system("pause");
+   return 0;
+}
+
